a48ab48edf0dfde53cd12a8140daa8cc21c4ccff
Gabriel Roger <gabriel.nroger@gmail.com>
2019-09-25 11:01:54 -0300

    Criar views para cadastro de coach

--- website/views.py
+++ website/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from website.models import *
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        data = Coach()
+        data.nome = request.POST['nome']
+        data.frase = request.POST['frase'] 
+        data.inspirador = request.POST['inspirador']
+        data.save()
+
+        args = {
+            'sucesso': 'Você conseguiu campeão! Grite: Alucinação!'
+        }
+
+        return render(request, 'index.html', args)
+
     return render(request, 'index.html') 
