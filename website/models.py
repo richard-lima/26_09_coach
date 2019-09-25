08abb7bf3825d17d8b1d1c7c3f00ca6b3832be38
Gabriel Roger <gabriel.nroger@gmail.com>
2019-09-25 11:01:22 -0300

    criar modelos e suas migrações

--- website/models.py
+++ website/models.py
@@ -4,6 +4,10 @@ from django.utils import timezone
 class Coach(models.Model):
     nome = models.CharField(max_length=255, verbose_name='Nome')
     frase = models.TextField()
-    inspirador = models.CharField(max_length=255, null=True)
+    inspirador = models.CharField(max_length=255, null=True, blank=True)
+    
     criado_em =  models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
